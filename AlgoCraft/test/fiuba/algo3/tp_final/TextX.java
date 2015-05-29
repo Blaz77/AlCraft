8aa66118b7486ef59ddde2054403f2f339b8ae53
Blaz77 <lugusor@gmail.com>
2015-05-29 18:23:03 -0300

    Proyecto Eclipse
    Proyecto preparado para Eclipse, siguiendo el Template del campus.
    Después podríamos separar en paquetes de Modelo, Vista y Controlador

---
+++ AlgoCraft/test/fiuba/algo3/tp_final/TextX.java
@@ -0,0 +1,18 @@
+package fiuba.algo3.tp_final;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class TextX {
+	
+	@Test
+	public void probarAlgo() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void probarOtraCosa() {
+		Assert.assertTrue(false);
+	}
+
+}
