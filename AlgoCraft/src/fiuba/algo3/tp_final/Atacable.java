15db613c27200b064e49302e6d3503c2b5b333c9
EmiSor <emilianosorbello@gmail.com>
2015-06-01 19:34:55 -0300

    Create Atacable.java

---
+++ AlgoCraft/src/fiuba/algo3/tp_final/Atacable.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.tp_final;
+
+public interface Atacable {
+
+	// Este podria ir a una interface con edificios.
+	public int getVida();
+
+	// Este podria ir a una interface con edificios.
+	public int getVidaMaxima();
+	
+	
+	// Este podria ir a una interface con edificios.
+	public void bajarVida(int puntos);
+	
+}
