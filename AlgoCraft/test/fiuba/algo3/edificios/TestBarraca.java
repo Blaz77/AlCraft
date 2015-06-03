b2562f73b0ab779f8bc73d64e8b7a53c7152a200
Blaz77 <lugusor@gmail.com>
2015-06-03 18:09:52 -0300

    Test Barraca

---
+++ AlgoCraft/test/fiuba/algo3/edificios/TestBarraca.java
@@ -0,0 +1,59 @@
+package fiuba.algo3.edificios;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fiuba.algo3.edificios.Edificio;
+import fiuba.algo3.tp_final.*;
+import fiuba.algo3.mapa.*;
+
+public class TestBarraca {
+
+	private Mapa mapa;
+	private Jugador jugador;
+	private EdificiosTerranFactory terranFactory;
+	private Edificio barraca;
+	
+	@Before
+	public void setUp() throws Exception {
+		//mapa = new Mapa(6);
+		this.jugador = new Jugador(TipoRaza.TERRAN, Color.AZUL);
+		this.terranFactory = new EdificiosTerranFactory();
+		this.barraca = terranFactory.crearEntrenadorUnidadesBasicas(jugador, 20, 40);
+	}
+
+	//TESTS SIN REQUISITOS POR AHORA!!!
+	
+	@Test
+	public void testCrearBarraca() {
+		assertEquals(barraca.getNombre(),"Barraca");
+	}
+	
+	@Test
+	public void testBarracaSubeVidaDuranteConstruccion() {
+		int vidaRelativa = barraca.getVida();
+		for(int i = 0; i < 4; i++){
+			barraca.pasarTurno();
+			if (barraca.getVida() <= vidaRelativa) 
+				fail("No aumenta la vida en la Construccion");
+			vidaRelativa = barraca.getVida();
+		}
+		assertEquals(vidaRelativa, barraca.getVidaMaxima());
+	}
+	/*
+	@Test
+	public void testBarracaEntrenaUnidad() {
+		for(int i = 0; i < 4; i++) centroMineral.pasarTurno();
+		int mineralRelativo = jugador.getMinerales();
+		
+		for(int i = 0; i < 10; i++){
+			mineralRelativo += 10;
+			centroMineral.pasarTurno();
+			if (jugador.getMinerales() != mineralRelativo)
+				fail("El centro de Minerales no esta recolectando Minerales (de a 10)");
+		}
+	}*/
+
+}
