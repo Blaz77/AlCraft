dae398a14d2828e5bdfc530fcbf2ece22c1646a2
EmiSor <emilianosorbello@gmail.com>
2015-06-04 13:04:13 -0300

    Update RazaFactory.java

--- AlgoCraft/src/fiuba/algo3/tp_final/RazaFactory.java
+++ AlgoCraft/src/fiuba/algo3/tp_final/RazaFactory.java
@@ -1,7 +1,8 @@
 package fiuba.algo3.tp_final;
 
 public class RazaFactory {
-	
+	// Un hashmap es mas portable y modificable que una cadena de ifs.
+	// TODO despues lo agrego. Emi.
 	public Raza getRaza(TipoRaza raza) {
 		if (raza == TipoRaza.TERRAN) {
 			return new RazaTerran();
