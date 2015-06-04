c31973cedc338ad7e7bd4a56fc30a7e6386a9d6a
EmiSor <emilianosorbello@gmail.com>
2015-06-04 11:48:50 -0300

    comment Juego.java

--- AlgoCraft/src/fiuba/algo3/tp_final/Juego.java
+++ AlgoCraft/src/fiuba/algo3/tp_final/Juego.java
@@ -1,5 +1,31 @@
 package fiuba.algo3.tp_final;
 
 public class Juego {
-
+	// Algunos atributos posibles:
+	// Turnos
+	// Mapa
+	// Jugadores (en lista? asi jugar con >2 es posible)
+	// Referencia a la interfaz? no creo...
+	// cosas extra como jugador actual, constantes como turnos maximos si los hay
+	// 
+	
+	// Metodos:
+	
+	//	- Juego(opciones) 	->		inicializa el juego pero sin jugadores (mas facil
+	//								agregarlos despues).
+	
+	//	- pasarTurno() 		->		ordena a todas las unidades/edificios que realicen
+	//								su accion de cambio de turno.
+	//	- eliminarJugador() ->		remueve a un jugador del ciclo de turnos si murio
+	
+	//	- hayGanador() 		->		Se fija si queda un jugador vivo nomas.
+	
+	
+	//	- de Delegacion (podrian no estar y q de arriba accedan a los objetos directamente):
+	
+	//		· ataque(pos1, pos2)
+	
+	//		· moverse(pos1, pos2)
+	
+	//		· construir(jugador, edif, pos)
 }
