a9c88fb99178850806b7752f857865f0eddfa517
Pablo Murck <pablomurck@gmail.com>
2015-06-05 16:43:01 -0300

    Creado VidaConEscudo (Protoss).

---
+++ AlgoCraft/src/fiuba/algo3/componentes/VidaConEscudo.java
@@ -0,0 +1,47 @@
+package fiuba.algo3.componentes;
+
+public class VidaConEscudo extends Vida {
+	
+	private int escudo;
+	private final int escudoMaximo;
+	
+	// Para setear especificamente la vida/escudo inicial (ej: construccion de edificios)
+	public VidaConEscudo(int vidaInicial, int vidaMaxima, int escudoInicial, int escudoMaximo) {
+		super(vidaInicial, vidaMaxima);
+		this.escudoMaximo = escudoMaximo;
+		this.escudo = escudoInicial;
+	}
+	
+	public VidaConEscudo(int vidaMaxima, int escudoMaximo){
+		this(vidaMaxima, vidaMaxima, escudoMaximo, escudoMaximo);
+	}
+
+	public boolean tieneEscudo(){
+		return true;
+	}
+
+	public int getEscudo(){
+		return this.escudo;
+	}
+	
+	public int getEscudoMaximo(){
+		return this.escudoMaximo;
+	}
+	
+	public void recibirDanio(int puntos){
+		this.escudo -= puntos;
+		if (this.escudo < 0){
+			super.recibirDanio(-(this.escudo)); //SUPER lanza la excepcion de muerte
+			this.escudo = 0;
+		}
+	}
+	
+	public Componente pasarTurno() {
+		//REGENERA 10% POR TURNO, SE PUEDE CAMBIAR, VER BIEN EL REAL
+		this.escudo += (this.escudoMaximo / 10);
+		if (this.escudo > this.escudoMaximo)
+			this.escudo = this.escudoMaximo;
+		return this;
+	}
+	
+}
