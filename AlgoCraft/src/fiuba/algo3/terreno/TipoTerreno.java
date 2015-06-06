89fedb7feeeb7f36f54384052d92f52dd7b778c7
emilianosorbello <emilianosorbello@gmail.com>
2015-06-06 17:18:52 -0300

    update TipoTerreno.java

--- AlgoCraft/src/fiuba/algo3/terreno/TipoTerreno.java
+++ AlgoCraft/src/fiuba/algo3/terreno/TipoTerreno.java
@@ -1,5 +1,33 @@
 package fiuba.algo3.terreno;
 
+// La idea de esto es eliminar las clases Terreno e hijas,
+// pues tenian poco comportamiento y nada de estado que
+// ameritara tener instancias de las mismas.
 public enum TipoTerreno {
-	TIERRA, ESPACIO
+	//      Edificable, Transitable, Sobrevolable
+	TIERRA (      true,        true,         true), 
+	ESPACIO(     false,       false,         true);
+	
+	private boolean esEdificable;
+	private boolean esTransitable;
+	private boolean esSobrevolable;
+
+	TipoTerreno(boolean ed, boolean tr, boolean sv){
+		this.esEdificable = ed;
+		this.esTransitable = tr;
+		this.esSobrevolable = sv;
+	}
+
+	public boolean esEdificable() {
+		return esEdificable;
+	}
+	
+	public boolean esTransitable() {
+		return esTransitable;
+	}
+	
+	public boolean esSobrevolable() {
+		return esSobrevolable;
+	}
+	
 }
