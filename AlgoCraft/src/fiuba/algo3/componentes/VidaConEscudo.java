4be4192813460116492381f5ad994f5b9507471f
EmiSor <emilianosorbello@gmail.com>
2015-06-11 12:05:35 -0300

    Update VidaConEscudo.java

--- AlgoCraft/src/fiuba/algo3/componentes/VidaConEscudo.java
+++ AlgoCraft/src/fiuba/algo3/componentes/VidaConEscudo.java
@@ -1,4 +1,3 @@
-
 package fiuba.algo3.componentes;
 
 import fiuba.algo3.atributos.AtributosObjetoVivo;
@@ -6,22 +5,23 @@ import fiuba.algo3.atributos.AtributosObjetoVivo;
 public class VidaConEscudo extends Vida {
 	
 	private int escudo;
+	private int escudoMaximo;
 	
 	// Para setear especificamente la vida/escudo inicial (ej: construccion de edificios)
 	public VidaConEscudo(int vidaInicial, int escudoInicial, AtributosObjetoVivo atributos) {
 		super(vidaInicial, atributos);
 		this.escudo = escudoInicial;
+		this.escudoMaximo = atributos.getEscudoMaximo();
 	}
 	
 	public VidaConEscudo(AtributosObjetoVivo atributos){
-		super(atributos);
-		this.escudo = this.atributos.getEscudoMaximo();
+		this(atributos.getVidaMaxima(), atributos.getEscudoMaximo(), atributos);
 	}
 
 	@Override
 	public boolean tieneEscudo(){
-		return atributos.tieneEscudo();
-		//return true;
+		//return atributos.tieneEscudo(); // Consideren que esta clase ya sabe q tiene escudo, y los atributos no los necesitaria para nada
+		return true;
 	}
 
 	@Override
@@ -31,21 +31,21 @@ public class VidaConEscudo extends Vida {
 	
 	@Override
 	public int getEscudoMaximo(){
-		return this.atributos.getEscudoMaximo();
+		return this.escudoMaximo;
 	}
 	
 	@Override
 	public void regenerarEscudo(int cantidad) {
 		this.escudo += cantidad;
-		if (this.escudo > atributos.getEscudoMaximo()) 
-			this.escudo = atributos.getEscudoMaximo();
+		if (this.escudo > getEscudoMaximo()) // escudo = escudo < escudoMaximo ? escudo : escudoMaximo;
+			this.escudo = getEscudoMaximo(); // Magia de C ^
 	}
 	
 	public void recibirDanio(int puntos){
 		this.escudo -= puntos;
 		if (this.escudo < 0){
 			super.recibirDanio(Math.abs(this.escudo)); //SUPER lanza la excepcion de muerte
-			this.escudo = 0;
+			this.escudo = 0; // ^ Esta linea es gloriosa
 		}
 	}
 	
