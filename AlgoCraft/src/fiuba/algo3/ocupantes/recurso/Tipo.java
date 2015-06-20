ef61288772d29051aa2277d2f3b0dca30c70e6f1
Pablo Murck <pablomurck@gmail.com>
2015-06-20 16:41:16 -0300

    Tipos mas especificos.

---
+++ AlgoCraft/src/fiuba/algo3/ocupantes/recurso/Tipo.java
@@ -0,0 +1,52 @@
+package fiuba.algo3.ocupantes.recurso;
+
+public enum Tipo { //Especie / Nombre
+	
+	// Especial:
+	DESCONOCIDO(TipoOcupante.DESCONOCIDO, "LAL"),
+	
+	// Recursos:
+	CELDA_VACIA(TipoOcupante.RECURSO, ""), //O CeldaVacia(TipoOcupante.CeldaVacia)
+	//NO_RECURSO(TipoOcupante.Celda_Vacia) en vez del de arriba
+	MINERAL(TipoOcupante.RECURSO, ""),
+	VESPENO(TipoOcupante.RECURSO, ""),
+	
+	// Unidades Terran:
+	MARINE(TipoOcupante.UNIDAD, "Marine"), //TipoRaza etc.
+	
+	// Edificios Terran:
+	CENTRO_DE_MINERAL(TipoOcupante.EDIFICIO, "Marine"),
+	REFINERIA(TipoOcupante.EDIFICIO, "Marine"),
+	DEPOSITO_SUMINISTRO(TipoOcupante.EDIFICIO, "Marine"), 
+	BARRACA(TipoOcupante.EDIFICIO, "Marine"),
+	FABRICA(TipoOcupante.EDIFICIO, "Marine"),
+	PUERTO_ESTELAR_TERRAN(TipoOcupante.EDIFICIO, "Marine"),
+	
+			
+	// Edificios Protoss
+	NEXO_MINERAL(TipoOcupante.EDIFICIO, "Marine"),
+	PILON(TipoOcupante.EDIFICIO, "Marine"),
+	ASIMILADOR(TipoOcupante.EDIFICIO, "Marine"),
+	ACCESO(TipoOcupante.EDIFICIO, "Marine"),
+	PUERTO_ESTELAR_PROTOSS(TipoOcupante.EDIFICIO, "Marine"),
+	ARCHIVOS_TEMPLARIOS(TipoOcupante.EDIFICIO, "Marine"),	
+	
+	;
+	
+	private TipoOcupante t;
+	private String s;
+	
+	Tipo(TipoOcupante t, String s){
+		this.t = t;
+		this.s = s;
+	}
+	
+	//					getSuperTipo
+	public TipoOcupante getTipoOcupante(){
+		return this.t;
+	}
+	
+	public String getNombre(){
+		return this.s;
+	}
+}
--- AlgoCraft/src/fiuba/algo3/ocupantes/recurso/TipoOcupante.java
+++ AlgoCraft/src/fiuba/algo3/ocupantes/recurso/TipoOcupante.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.ocupantes.recurso;
 
 public enum TipoOcupante {
-	CELDA_VACIA, MINERAL, VESPENO,
-	UNIDAD, EDIFICIO, DESCONOCIDO,
+		
+	RECURSO, UNIDAD, EDIFICIO, DESCONOCIDO;
+	
 }
