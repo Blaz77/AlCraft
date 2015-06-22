2fce709ada63ee82e2a1e6026ad5dae8aeba0282
Pablo Murck <pablomurck@gmail.com>
2015-06-22 20:09:16 -0300

    detalle

--- AlgoCraft/src/fiuba/algo3/excepciones/GasVespenoInsuficiente.java
+++ AlgoCraft/src/fiuba/algo3/excepciones/GasVespenoInsuficiente.java
@@ -3,7 +3,7 @@ package fiuba.algo3.excepciones;
 public class GasVespenoInsuficiente extends RuntimeException {
 
 	public GasVespenoInsuficiente(int costo, int actual){
-		super(String.format("Recurso Mineral Insuficiente.\nCosto: %d\nNecesita: %d", 
+		super(String.format("Recurso Gas Vespeno Insuficiente.\nCosto: %d\nNecesita: %d", 
 			costo, costo-actual));
 	}
 }
